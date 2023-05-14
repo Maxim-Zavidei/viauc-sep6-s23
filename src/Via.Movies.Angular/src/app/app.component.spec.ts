aa17a1236007298aa94e274c8af3845dcf68ad71
LilianBittar <lilian.bittar@pm.me>
2023-05-14 22:35:34 +0200

    Removed unnessasry test.

--- src/Via.Movies.Angular/src/app/app.component.spec.ts
+++ src/Via.Movies.Angular/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Via.Movies.Angular');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Via.Movies.Angular app is running!');
-  });
 });
