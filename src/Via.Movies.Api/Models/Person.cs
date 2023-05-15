30354b102c583b0066fda406e68a390df3d49968
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-05-15 10:29:09 +0200

    Created the models

---
+++ src/Via.Movies.Api/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Via.Movies.Api.Models;
+
+public partial class Person
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required long Birth { get; set; }
+}
---
+++ src/Via.Movies.Api/Models/Star.cs
@@ -0,0 +1,7 @@
+﻿namespace Via.Movies.Api.Models;
+
+public partial class Star
+{
+    public required int MovieId { get; set; }
+    public required int PersonId { get; set; }
+}
