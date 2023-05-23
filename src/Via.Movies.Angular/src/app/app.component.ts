376398b89ab159d6ec1000adee23f6d057a8afcf
LilianBittar <lilian.bittar@pm.me>
2023-05-23 19:07:45 +0200

    Added person service + director.

--- src/Via.Movies.Angular/src/app/app.component.ts
+++ src/Via.Movies.Angular/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { ApiAdapterService } from './services/api-adapter.service';
 import { MovieService } from './services/movie.service';
 
 @Component({
@@ -17,44 +16,21 @@ export class AppComponent {
   movies: any[] = [];
   persons: any[] = [];
   directors: any[] = [];
-
-  // persons: any[] = [
-  //   // create an array of persons with Id, Name, and Birth.
-  //     { Id: 1, Name: 'John Doe', Birth: '01/01/1970' },
-  //     { Id: 2, Name: 'Nicola Johnson', Birth: '05/04/1980' },
-  //     { Id: 3, Name: 'Lesh Smith', Birth: '11/10/1967' }
-  //     ];
-    
-  //     directors: any[] = [
-  //   // create an array of directors with MovieId and PersonId.
-  //      { MovieId: 1, PersonId: 1 },
-  //       { MovieId: 2, PersonId: 2 },
-  //       { MovieId: 3, PersonId: 3 }
-  //     ];
-    
-  //     movies: any[] = [
-  //   // create an array of movies with Id, Title and Year.
-  //       { Id: 1, Title: 'The Shawshank Redemption', Year: 1994 },
-  //       { Id: 2, Title: 'The Godfather', Year: 1972 },
-  //       { Id: 3, Title: 'The Godfather: Part II', Year: 1974 }
-  //     ];
-  //       
-
   title = 'Via.Movies.Angular';
 
 
   cachedList: any[] = [];
 
-	constructor(private movieService: MovieService, private apiAdapter: ApiAdapterService) { }
+	constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
-    this.apiAdapter.getAllMovies().subscribe((data: any[]) => {
+    this.movieService.getMovies().subscribe((data: any[]) => {
       this.movies = data;
       this.cachedList = this.movies;
     });
-    for (let director of this.directors) {
-      this.movies.find(movie => movie.Id === director.MovieId).Director = this.persons.find(person => person.Id === director.PersonId).Name;
-    }
+    // for (let director of this.directors) {
+    //   this.movies.find(movie => movie.Id === director.MovieId).Director = this.persons.find(person => person.Id === director.PersonId).Name;
+    // }
   }
 
   findMovie(searchText: string) {
