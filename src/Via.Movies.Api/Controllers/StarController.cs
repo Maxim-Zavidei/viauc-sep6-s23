69ccf2fe947095ff29f0777ff15b5553cf5a6192
LilianBittar <lilian.bittar@pm.me>
2023-05-26 18:48:27 +0200

    Created star controller.

--- src/Via.Movies.Api/Controllers/StarController.cs
+++ src/Via.Movies.Api/Controllers/StarController.cs
@@ -1,42 +1,32 @@
-// using System.Net.Mime;
-// using Microsoft.AspNetCore.Mvc;
-// using Via.Movies.Api.Dtos;
-// using Via.Movies.Api.Models;
-// using Via.Movies.Api.Repositories;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Via.Movies.Api.Dtos;
+using Via.Movies.Api.Models;
+using Via.Movies.Api.Repositories;
 
-// namespace Via.Movies.Api.Controllers;
+namespace Via.Movies.Api.Controllers;
 
-// [ApiController]
-// [Route("api/[controller]")]
-// public class StarController : ControllerBase
-// {
-// 	private IStarRepository starRepository;
+[ApiController]
+[Route("api/[controller]")]
+public class StarController : ControllerBase
+{
+	private IStarRepository starRepository;
 
-// 	public StarController(IStarRepository starRepository)
-// 	{
-// 		this.starRepository = starRepository;
-// 	}
+	public StarController(IStarRepository starRepository)
+	{
+		this.starRepository = starRepository;
+	}
 
-// 	[HttpGet]
-// 	public async Task<ActionResult<IEnumerable<Star>>> GetAllStarsAsync()
-// 	{
-// 		return Ok(await starRepository.GetAllStarsAsync());
-// 	}
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Star>>> GetAllStarsAsync()
+	{
+		return Ok(await starRepository.GetAllStarsAsync());
+	}
 
-// 	[HttpPut]
-//     [Consumes(MediaTypeNames.Application.Json)]
-//     public async Task<ActionResult<Star>> AddStarAsync([FromBody] AddStarRequest request)
-//     {
-// 		var star = await starRepository.CreateStarAsync(new Star
-// 		{
-// 			MovieId = request.MovieId,
-// 			PersonId = request.PersonId
-// 		});
+    [HttpGet("{movieId}")]
+    public async Task<IEnumerable<Star>> GetStarsForMovieAsync(long movieId)
+    {
+        return await starRepository.GetStarsForMovieAsync(movieId);
+    }
 
-// 		if (star == null)
-//         {
-//             return BadRequest("A person with the provided person id does not exist or a movie with the provided movie id does not exist.");
-//         }
-//         return Ok(star);
-//     }
-// }
+}
